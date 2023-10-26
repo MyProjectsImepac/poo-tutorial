4100c3d2695b8545e27b4c99a921956d1697890c
everton <evertonhf@gmail.com>
2023-10-26 20:12:45 -0300

    Application initial

---
+++ src/main/java/br/edu/imepac/spring/tutorial/SpringTutorial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.imepac.spring.tutorial;
+
+/**
+ *
+ * @author ehf_v
+ */
+public class SpringTutorial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
